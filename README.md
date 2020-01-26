ab4a7d08d55d62361f01d2511179d88ebeb22223
Matthew S Giancola <matthew.giancola42@gmail.com>
2020-01-25 20:18:57 -0500

    most basic web handles

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # conuhacksv
 repo for CONUHACKS V
+
+web handles to make
+FROM USER COLLECTION
+-Admin Only 
+--addTask: add a task to this group 1
+--editTask: edit a task in this group 2, uses code
+--removeTask: remove a task from this group 3
+--addUser: add a user to this group 4
+--removeUser: remove a user from this group 5
+-User 
+--claimTask: claim a task for a specific user (expand to many later on) 6
+--endTask: stop doing this task, it and subtasks are sent back in* 7
+--completeTask: complete this task/subtask 8
+--splitTask: splits the task into movable subtasks 9
+userData: returns the user data in a printable way? 10
+updateData: update the user data 11
+
+FROM GROUP & TASK COLLECTION
+--findOpenTasks: gets all the tasks from this group which you can do as they have not been claimed 12
+--findUserTasks: gets the tasks that the user is trying to get done from the database 13
+--validateTask: makes sure that the time values for the task are done up properly based on resize on the front end (moving and such on the cal) 14
+--findSplitData: how many splits does the current task actually have(single user may not be needed) 14
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var Cookies = require('cookies');
 var keys = ['keyboard cat'];
 
 
+
+
 //database setup
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Dev:<password>@conuhacks-g1c1s.mongodb.net/test?retryWrites=true&w=majority";
@@ -35,39 +37,17 @@ client.connect(err => {
 
   //START: Actual Event Handlers
 
-  //Make a user
-  var aNewUserHandler = function () {
-    console.log('EVENT: NewUser');
-  }
-  //Assign the event handler to an event:
-  eventEmitter.on('newUser', aNewUserHandler);
-
-  //Make a group
-  var aNewGroupHandler = function () {
-    console.log('EVENT: NewGroup');
-  }
-  //Assign the event handler to an event:
-  eventEmitter.on('newGroup', aNewGroupHandler);
-
-  //Make a task
-  var aNewTaskHandler = function () {
-    console.log('EVENT: NewGroup');
-  }
-  //Assign the event handler to an event:
-  eventEmitter.on('newTask', aNewGroupHandler);
 
   http.createServer(function (req, res) {
     //make the cookies object
     var cookies = new Cookies(req, res, { keys: keys });
-
-    /*
-  // Get a cookie
-  var lastVisit = cookies.get('LastVisit', { signed: true })
-
-  // Set the cookie to a value
-  cookies.set('LastVisit', new Date().toISOString(), { signed: true })
-    
-  */
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    tasks = 1;
+    users = 1;
+    //START: General Varables for the cookies that we need
+    var userCode = cookies.get('UserCode', { signed: true })
+    var iAmAdmin = cookies.get('IAmAdmin', { signed: true })
 
   console.log(req.method);
   if (req.method=="POST"){
@@ -86,8 +66,124 @@ client.connect(err => {
               var post = qs.parse(body);
               // use post['blah'], etc.
               console.log(post['to']);
-              if(post['to']=='test'){
-                eventEmitter.emit('test');
+             
+              if(filename.includes("event")){
+                if (iAmAdmin){
+                  //addTask
+                  if(filename.includes("addTask")){
+                    post['code']= tasks++//gets the newest 
+                    post['parent'] = -1;
+                    taskCollection.insertOne(post, function(err, res) {
+                      if (err) throw err;
+                      console.log("1 document inserted");
+                    });
+                  }
+                  //editTask
+                  if(filename.includes("editTask")){
+                    var query =  {code:post['code']};
+                    found = taskCollection.updateOne(query,post, function(err, res){
+                      if (err) throw err;
+                      console.log("1 document updated");
+                    });
+                  }
+                  //removeTask
+                  if(filename.includes("removeTask")){
+                    var query =  {code:post['code']};
+                    found = taskCollection.deleteOne(query, function(err, res){
+                      if (err) throw err;
+                      console.log("1 document removed");
+                    });
+                  }
+                  //removeUser
+                  if(filename.includes("removeUser")){
+                    var query =  {code:post['code']};
+                    found = userCollection.deleteOne(query, function(err, res){
+                      if (err) throw err;
+                      console.log("1 document Removed");
+                    });
+                  }
+                  //addUser
+                  if(filename.includes("addUser")){
+                    post['code']= users++//gets the newest 
+                    userCollection.insertOne(post, function(err, res) {
+                      if (err) throw err;
+                      console.log("1 document inserted");
+                    });
+                  }
+                }
+                  //claim task (user calls, updates task)
+                  if(filename.includes("claimTask")){
+                    var query= {code:post['code'], status:false};//gets the newest 
+                    var claims =  {worker:userCode};
+                    found = taskCollection.updateOne(query,claims, function(err, res){
+                      if (err) throw err;
+                      console.log("1 document updated");
+                    });
+                  }
+                  //end task (user calls, updates task)
+                  if(filename.includes("endTask")){
+                    var query= {code:post['code'], status:false};//gets the newest 
+                    var claims =  {worker:-1};
+                    found = taskCollection.updateOne(query,claims, function(err, res){
+                      if (err) throw err;
+                      console.log("1 document updated");
+                    });
+                  }
+                  //complete task (user calls, updates task)
+                  if(filename.includes("completeTask")){
+                    var query= {code:post['code'], status:false};//gets the newest 
+                    var claims =  {status:true};
+                    found = taskCollection.updateOne(query,claims, function(err, res){
+                      if (err) throw err;
+                      console.log("1 document updated");
+                    });
+                  }
+                  //split task
+                  if(filename.includes("splitTask")){
+                    var query= {code:post['code'], status:false};//gets the newest 
+                    var nextSplit = post['split']+1;
+                    var claims =  {status:nextSplit};
+                    found = taskCollection.updateOne(query,claims, function(err, res){
+                      if (err) throw err;
+                      console.log("1 document updated");
+                    });
+                  }
+                  //userData
+                  if(filename.includes("userData")){
+                    var query= {code:userCode};//gets the newest (should output the values directly)
+                    found = taskCollection.findOne(query, function(err, res){
+                      if (err) throw err;
+                      console.log("1 document updated");
+                    });
+                  }
+                  //updateData
+                  if(filename.includes("updateData")){
+                    var query =  {code:post['code']};
+                    found = userCollection.updateOne(query,post, function(err, res){
+                      if (err) throw err;
+                      console.log("1 document updated");
+                    });
+                  }
+                  //findOpenTasks
+                  if(filename.includes("userData")){
+                    var query= {status:false, worker:-1};//gets the newest (should output the values directly)
+                    found = taskCollection.find(query).toArray(function(err, res){
+                      if (err) throw err;
+                      console.log("1 document updated");
+                    });
+                  }
+                  //findUserTasks
+                  if(filename.includes("findUserTasks")){
+                    var query= {status:false, worker:userCode};//gets the newest (should output the values directly)
+                    found = taskCollection.find(query).toArray(function(err, res){
+                      if (err) throw err;
+                      console.log("1 document updated");
+                    });
+                  }
+                  //findUserTasks
+                  if(filename.includes("validate")){
+                    console.log("DUMMY");
+                  }
               }
             //PROCESS EVERYTHING OUT AS NEDED
 
@@ -95,15 +191,13 @@ client.connect(err => {
     }
     if (req.method=="GET"||req.method=="POST"){
       //get or post only, always check events
-      var q = url.parse(req.url, true);
-      var filename = "." + q.pathname;
       console.log(filename);
       //register the different end points, this must be first
       if (filename=="./"||filename.includes("index")){
           filename = "./index.html";
       }
       //the event rule to remove extra and send to old
-      if(filename.includes("index")){
+      if(filename.includes("event")){
         filename = "./index.html";
       }
    
